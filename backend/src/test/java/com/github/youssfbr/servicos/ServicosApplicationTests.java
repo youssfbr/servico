423a35c5e8db0189f3e6de78fabab76211ad2698
Alisson Youssf <youssfbr@gmail.com>
2021-11-11 18:02:36 -0300

    Project backend created

---
+++ backend/src/test/java/com/github/youssfbr/servicos/ServicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.servicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
