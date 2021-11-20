3d33196864d4bac5fe4d53125ccf1188b08a177e
Alisson Youssf <youssfbr@gmail.com>
2021-11-19 21:37:15 -0300

    Angular: ServiceForm comboBox

--- frontweb-ang/src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ frontweb-ang/src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Client } from './../../shared/types/client';
+
+import { ClientsService } from '../../shared/services/clients.service';
+
 @Component({
   selector: 'app-servico-prestado-form',
   templateUrl: './servico-prestado-form.component.html',
-  styleUrls: ['./servico-prestado-form.component.css']
+  styleUrls: ['./servico-prestado-form.component.css'],
 })
 export class ServicoPrestadoFormComponent implements OnInit {
+  clients: Client[] = [];
 
-  constructor() { }
+  constructor(private clienteService: ClientsService) {}
 
   ngOnInit(): void {
+    this.clienteService.getClients().subscribe(
+      response => this.clients = response,
+      error => console.error(error)
+    );
+  }
+
+  onSubmit(): void {
+    console.log('submit');
   }
 
 }
--- frontweb-ang/src/app/servico-prestado/servico-prestado-routing.module.ts
+++ frontweb-ang/src/app/servico-prestado/servico-prestado-routing.module.ts
@@ -1,7 +1,13 @@
+import { ServicoPrestadoListaComponent } from './servico-prestado-lista/servico-prestado-lista.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ServicoPrestadoFormComponent } from './servico-prestado-form/servico-prestado-form.component';
+
+const routes: Routes = [
+  { path: 'servico-prestado-form', component: ServicoPrestadoFormComponent},
+  { path: 'servico-prestado-lista', component: ServicoPrestadoListaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
